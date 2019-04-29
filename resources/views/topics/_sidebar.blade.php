b85b754a40a952c2721e6ea6d16a67045e1cc821
echo <echoe.g@yahoo.com>
2019-04-29 18:50:44 +0800

    用户可以创建话题

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,1 +1,7 @@
-youbiandao
+<div class="card ">
+    <div class="card-body">
+        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
+            <i class="fas fa-pencil-alt mr-2"></i>  新建帖子
+        </a>
+    </div>
+</div>
