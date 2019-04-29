3c174cbbe3e45646fd1d2e40240e1b92e81fe6c4
echo <echoe.g@yahoo.com>
2019-04-29 18:39:37 +0800

    话题排序

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,4 +1,1 @@
-<a class="text-secondary" href="{{ route('categories.show', $topic->category_id) }}" title="{{ $topic->category->name }}">
-    <i class="far fa-folder"></i>
-    {{ $topic->category->name }}
-</a>
+youbiandao
